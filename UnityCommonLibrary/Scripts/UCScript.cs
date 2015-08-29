b7a90fc167238d82d916fc473a3293b45a4b0886
Michael Notarnicola <michael@beardphantom.com>
2015-08-29 03:51:56 -0400

    Changed up invoke execution.

--- UnityCommonLibrary/Scripts/UCScript.cs
+++ UnityCommonLibrary/Scripts/UCScript.cs
@@ -1,76 +1,23 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace UnityCommonLibrary {
     [Serializable]
     public abstract class UCScript : MonoBehaviour {
 
-        public Coroutine Exec(IEnumerator ie) {
-            return StartCoroutine(ie);
+        public void TSCancelInvoke(Action a) {
+            CancelInvoke(a.Method.Name);
         }
 
         public void TSInvoke(Action a, float time) {
-            Exec(_TSInvoke(a, time, false));
+            Invoke(a.Method.Name, time);
         }
 
-        public void TSInvoke(Action a, float time, bool unscaled) {
-            Exec(_TSInvoke(a, time, unscaled));
+        public void TSCancelInvoke<T>(Action<T> a) {
+            CancelInvoke(a.Method.Name);
         }
 
-        public void TSInvoke<T>(Action<T> a, T arg, float time) {
-            Exec(_TSInvoke(a, arg, time, false));
-        }
-
-        public void TSInvoke<T>(Action<T> a, T arg, float time, bool unscaled) {
-            Exec(_TSInvoke(a, arg, time, unscaled));
-        }
-
-        public void TSInvoke(IEnumerator a, float time) {
-            Exec(_TSInvoke(a, time, false));
-        }
-
-        public void TSInvoke(IEnumerator a, float time, bool unscaled) {
-            Exec(_TSInvoke(a, time, unscaled));
-        }
-
-        IEnumerator _TSInvoke(IEnumerator a, float time, bool unscaled) {
-            if(unscaled) {
-                var start = Time.unscaledTime;
-                while(Time.unscaledTime - start < time) {
-                    yield return null;
-                }
-            }
-            else {
-                yield return new WaitForSeconds(time);
-            }
-            Exec(a);
-        }
-
-        IEnumerator _TSInvoke(Action a, float time, bool unscaled) {
-            if(unscaled) {
-                var start = Time.unscaledTime;
-                while(Time.unscaledTime - start < time) {
-                    yield return null;
-                }
-            }
-            else {
-                yield return new WaitForSeconds(time);
-            }
-            a();
-        }
-
-        IEnumerator _TSInvoke<T>(Action<T> a, T arg, float time, bool unscaled) {
-            if(unscaled) {
-                var start = Time.unscaledTime;
-                while(Time.unscaledTime - start < time) {
-                    yield return null;
-                }
-            }
-            else {
-                yield return new WaitForSeconds(time);
-            }
-            a(arg);
-        }
     }
 }
