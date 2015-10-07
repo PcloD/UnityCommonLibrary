95352d22d9b48628a5d496bd0ad3b1d808d1585b
Michael Notarnicola <michael@beardphantom.com>
2015-10-07 03:08:51 -0400

    Removed nonsupported scripts.

--- UnityCommonLibrary/Scripts/ListExtensions.cs
+++ UnityCommonLibrary/Scripts/ListExtensions.cs
@@ -6,10 +6,10 @@ namespace UnityCommonLibrary {
 
         public static void Shuffle<T>(this IList<T> list) {
             var random = new Random();
-            int n = list.Count;
+            var n = list.Count;
             while(n > 1) {
                 n--;
-                int k = random.Next(n + 1);
+                var k = random.Next(n + 1);
                 T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
--- UnityCommonLibrary/Scripts/TransformUtility.cs
+++ UnityCommonLibrary/Scripts/TransformUtility.cs
@@ -5,35 +5,35 @@ namespace UnityCommonLibrary {
 
         public static void SetPosition(this Transform transform, float? x = null, float? y = null, float? z = null) {
             var newV3 = transform.position;
-            SetV3Parts(ref newV3, x, y, z);
+            newV3.SetXYZ(x, y, z);
             transform.position = newV3;
         }
 
         public static void SetLocalPosition(this Transform transform, float? x = null, float? y = null, float? z = null) {
             var newV3 = transform.localPosition;
-            SetV3Parts(ref newV3, x, y, z);
+            newV3.SetXYZ(x, y, z);
             transform.localPosition = newV3;
         }
 
         public static void SetLocalScale(this Transform transform, float? x = null, float? y = null, float? z = null) {
             var newV3 = transform.localScale;
-            SetV3Parts(ref newV3, x, y, z);
+            newV3.SetXYZ(x, y, z);
             transform.localScale = newV3;
         }
 
         public static void SetEulerAngles(this Transform transform, float? x = null, float? y = null, float? z = null) {
             var newV3 = transform.eulerAngles;
-            SetV3Parts(ref newV3, x, y, z);
+            newV3.SetXYZ(x, y, z);
             transform.rotation = Quaternion.Euler(newV3);
         }
 
         public static void SetLocalEulerAngles(this Transform transform, float? x = null, float? y = null, float? z = null) {
             var newV3 = transform.localEulerAngles;
-            SetV3Parts(ref newV3, x, y, z);
+            newV3.SetXYZ(x, y, z);
             transform.localRotation = Quaternion.Euler(newV3);
         }
 
-        static void SetV3Parts(ref Vector3 v3, float? x = null, float? y = null, float? z = null) {
+        public static void SetXYZ(this Vector3 v3, float? x = null, float? y = null, float? z = null) {
             if(x.HasValue) {
                 v3.x = x.Value;
             }
@@ -42,6 +42,21 @@ namespace UnityCommonLibrary {
             }
             if(z.HasValue) {
                 v3.z = z.Value;
+            }
+        }
+
+        public static void SetWXYZ(this Quaternion q, float? w = null, float? x = null, float? y = null, float? z = null) {
+            if(w.HasValue) {
+                q.w = w.Value;
+            }
+            if(x.HasValue) {
+                q.x = x.Value;
+            }
+            if(y.HasValue) {
+                q.y = y.Value;
+            }
+            if(z.HasValue) {
+                q.z = z.Value;
             }
         }
 
