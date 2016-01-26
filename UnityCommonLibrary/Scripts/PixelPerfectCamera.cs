eb565b7ba14520c76daecd0a97b281462ca9b2c6
Michael Notarnicola <michael@beardphantom.com>
2016-01-26 09:33:54 -0500

    PixelPerfectCamera

---
+++ UnityCommonLibrary/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace UnityCommonLibrary {
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    public class PixelPerfectCamera : MonoBehaviour {
+        public bool useCurrentRes;
+        public int screenWidth;
+        public int screenHeight;
+        public float pixelsPerUnit;
+
+        private new Camera camera;
+
+        private void Awake() {
+            camera = GetComponent<Camera>();
+        }
+
+        private void Update() {
+            float screenWidth = useCurrentRes ? Screen.currentResolution.width : this.screenWidth;
+            float screenHeight = useCurrentRes ? Screen.currentResolution.height : this.screenHeight;
+            camera.orthographicSize = screenWidth / (((screenWidth / screenHeight) * 2f) * pixelsPerUnit);
+        }
+    }
+}
