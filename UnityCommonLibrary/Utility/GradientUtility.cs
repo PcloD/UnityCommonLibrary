0f17ce4c3218ee0ec7777c63baaa8c3b789e9f76
Michael Notarnicola <michael@beardphantom.com>
2016-02-22 14:48:53 -0500

    GradientUtility SetColors, MakeMeter, SetAlphas

--- UnityCommonLibrary/Utility/GradientUtility.cs
+++ UnityCommonLibrary/Utility/GradientUtility.cs
@@ -4,16 +4,42 @@ namespace UnityCommonLibrary.Utilities {
     public static class GradientUtility {
 
         public static Gradient MakeRainbow(this Gradient g) {
-            g.colorKeys = new GradientColorKey[] {
-                new GradientColorKey(new Color(1f, 0f, 0f), 0f),
-                new GradientColorKey(new Color(1f, 1f, 0f), 0.25f),
-                new GradientColorKey(new Color(0f, 1f, 0f), 0.5f),
-                new GradientColorKey(new Color(0f, 1f, 1f), 0.75f),
-                new GradientColorKey(new Color(1f, 0f, 1f), 1f),
-            };
+            g.SetColors(new Color(1f, 0f, 0f),
+                        new Color(1f, 1f, 0f),
+                        new Color(0f, 1f, 0f),
+                        new Color(0f, 1f, 1f),
+                        new Color(1f, 0f, 1f));
             g.alphaKeys = new GradientAlphaKey[] {
                 new GradientAlphaKey(1f, 0f)
             };
+            return g;
+        }
+
+        public static Gradient MakeMeter(this Gradient g) {
+            g.SetColors(Color.red, Color.yellow, Color.green);
+            g.alphaKeys = new GradientAlphaKey[] {
+                new GradientAlphaKey(1f, 0f)
+            };
+            return g;
+        }
+
+        public static Gradient SetColors(this Gradient g, params Color[] colors) {
+            var interval = 1f / (colors.Length - 1);
+            g.colorKeys = new GradientColorKey[colors.Length];
+            for(int i = 0; i < colors.Length; i++) {
+                var t = Mathf.Clamp01(i * interval);
+                g.colorKeys[i] = new GradientColorKey(colors[i], t);
+            }
+            return g;
+        }
+
+        public static Gradient SetAlphas(this Gradient g, params float[] alphas) {
+            var interval = 1f / (alphas.Length - 1);
+            g.alphaKeys = new GradientAlphaKey[alphas.Length];
+            for(int i = 0; i < alphas.Length; i++) {
+                var t = Mathf.Clamp01(i * interval);
+                g.alphaKeys[i] = new GradientAlphaKey(alphas[i], t);
+            }
             return g;
         }
 
