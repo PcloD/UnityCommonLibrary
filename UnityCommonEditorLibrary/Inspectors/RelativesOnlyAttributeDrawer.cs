94d97f68205143ca96b798739ceaaa0b7c60125b
Michael Notarnicola <michael@beardphantom.com>
2016-03-13 23:33:44 -0400

    Relatives only attribute

---
+++ UnityCommonEditorLibrary/Inspectors/RelativesOnlyAttributeDrawer.cs
@@ -0,0 +1,53 @@
+﻿using UnityCommonLibrary.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityCommonEditorLibrary.Inspectors {
+	[CustomPropertyDrawer(typeof(RelativesOnlyAttribute))]
+	public class RelativesOnlyAttributeDrawer : PropertyDrawer {
+		private const string MSG = "This object field only allows assignments from the following relatives:\n\n{0}\n\nThe current value will be unassigned.";
+
+		private RelativesOnlyAttribute target;
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+			EditorGUI.PropertyField(position, property, label);
+
+			var component = property.objectReferenceValue as Component;
+			if(component != null) {
+				target = attribute as RelativesOnlyAttribute;
+				var isValid = ProcessRules(property, component.gameObject);
+				if(!isValid) {
+					EditorUtility.DisplayDialog("Invalid Reference", string.Format(MSG, target.validRelatives), "OK");
+					property.objectReferenceValue = null;
+				}
+			}
+		}
+
+		private bool ProcessRules(SerializedProperty property, GameObject obj) {
+			var propertyGameObject = (property.serializedObject.targetObject as Component).gameObject;
+
+			// First check hard-set rules
+			if(target.IsOnlyRuleSet(ValidRelatives.SameGameObject)) {
+				return obj == propertyGameObject;
+			}
+			if(target.IsOnlyRuleSet(ValidRelatives.Children)) {
+				return obj.transform.IsChildOf(propertyGameObject.transform) && obj != propertyGameObject;
+			}
+			if(target.IsOnlyRuleSet(ValidRelatives.Parents)) {
+				return propertyGameObject.transform.IsChildOf(obj.transform) && obj != propertyGameObject;
+			}
+
+			// Then check multi-applicable rules
+			if(target.IsRuleSet(ValidRelatives.SameGameObject) && obj == propertyGameObject) {
+				return true;
+			}
+			if(target.IsRuleSet(ValidRelatives.Children) && obj.transform.IsChildOf(propertyGameObject.transform) && obj != propertyGameObject) {
+				return true;
+			}
+			if(target.IsRuleSet(ValidRelatives.Parents) && propertyGameObject.transform.IsChildOf(obj.transform) && obj != propertyGameObject) {
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ UnityCommonLibrary/Attributes/RelativesOnlyAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityCommonLibrary.Attributes {
+	[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+	public sealed class RelativesOnlyAttribute : PropertyAttribute {
+		public ValidRelatives validRelatives { get; private set; }
+
+		public RelativesOnlyAttribute() {
+			validRelatives = ValidRelatives.SameHierarchyPath;
+		}
+
+		public RelativesOnlyAttribute(ValidRelatives validRelatives) {
+			this.validRelatives = validRelatives;
+		}
+
+		public bool IsRuleSet(ValidRelatives rule) {
+			return (validRelatives & rule) != 0;
+		}
+
+		public bool IsOnlyRuleSet(ValidRelatives rule) {
+			return validRelatives == rule;
+		}
+
+	}
+
+	[Flags]
+	public enum ValidRelatives {
+		SameGameObject = 1 << 0,
+		Children = 1 << 1,
+		Parents = 1 << 2,
+		SameHierarchyPath = SameGameObject | Children | Parents
+	}
+}
