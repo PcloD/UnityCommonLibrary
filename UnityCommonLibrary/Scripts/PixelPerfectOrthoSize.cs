5fa082802f3b898bf077588c953b666cbc6d3638
Michael Notarnicola <michael@beardphantom.com>
2016-03-19 12:39:15 -0400

    PixelPerfectOrthoSize

---
+++ UnityCommonLibrary/Scripts/PixelPerfectOrthoSize.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace UnityCommonLibrary {
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(Camera))]
+	public class PixelPerfectOrthoSize : MonoBehaviour {
+		public int designWidth;
+		public int designHeight;
+		public bool useWidth;
+		public float pixelsPerUnit;
+
+		private new Camera camera;
+
+		private void Awake() {
+			camera = GetComponent<Camera>();
+		}
+
+		private void Update() {
+			if(!useWidth && designHeight != 0f) {
+				camera.orthographicSize = (designHeight / pixelsPerUnit) / 2f;
+			}
+			else if(useWidth && designWidth != 0f) {
+				camera.orthographicSize = (designWidth / pixelsPerUnit) / 2f;
+			}
+		}
+	}
+}
