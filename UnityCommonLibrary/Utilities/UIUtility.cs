061b07ef97c1f0a56f82a1bee4fdabe7d7c2f7c2
Michael Notarnicola <michael@beardphantom.com>
2016-09-28 15:45:27 -0400

    Utilities

---
+++ UnityCommonLibrary/Utilities/UIUtility.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UnityCommonLibrary.Utilities
+{
+	public static class UIUtility
+	{
+		public static void SetPivot(this RectTransform rectTransform, Vector2 pivot)
+		{
+			var center = rectTransform.GetWorldCenter();
+			rectTransform.pivot = pivot;
+			rectTransform.position += (Vector3)(center - rectTransform.GetWorldCenter());
+		}
+		public static Vector2 GetWorldCenter(this RectTransform rectTransform)
+		{
+			return rectTransform.TransformPoint(rectTransform.rect.center);
+		}
+		public static Rect GetWorldRect(this RectTransform rectTransform)
+		{
+			return new Rect(rectTransform.TransformPoint(rectTransform.rect.min), rectTransform.rect.size);
+		}
+	}
+}
