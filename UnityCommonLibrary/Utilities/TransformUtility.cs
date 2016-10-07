064d0a930461b1baa98a78e3662780e9f6c2cc22
Michael Notarnicola <michael@beardphantom.com>
2016-10-07 03:54:12 -0400

    FindChildDeep

--- UnityCommonLibrary/Utilities/TransformUtility.cs
+++ UnityCommonLibrary/Utilities/TransformUtility.cs
@@ -2,81 +2,95 @@
 
 namespace UnityCommonLibrary.Utilities
 {
-	public static class TransformUtility
-	{
-		public static void SetPosition(this Transform transform, Space space, float? x = null, float? y = null, float? z = null)
-		{
-			var newV3 = transform.position;
-			newV3.SetXYZ(x, y, z);
-			if (space == Space.World)
-			{
-				transform.position = newV3;
-			}
-			else
-			{
-				transform.localPosition = newV3;
-			}
-		}
-		public static void SetLocalScale(this Transform transform, float? x = null, float? y = null, float? z = null)
-		{
-			var newV3 = transform.localScale;
-			newV3.SetXYZ(x, y, z);
-			transform.localScale = newV3;
-		}
-		public static void SetEulerAngles(this Transform transform, Space space, float? x = null, float? y = null, float? z = null)
-		{
-			var newV3 = transform.eulerAngles;
-			newV3.SetXYZ(x, y, z);
-			if (space == Space.World)
-			{
-				transform.rotation = Quaternion.Euler(newV3);
-			}
-			else
-			{
-				transform.localRotation = Quaternion.Euler(newV3);
-			}
-		}
+    public static class TransformUtility
+    {
+        public static void SetPosition(this Transform transform, Space space, float? x = null, float? y = null, float? z = null)
+        {
+            var newV3 = transform.position;
+            newV3.SetXYZ(x, y, z);
+            if(space == Space.World)
+            {
+                transform.position = newV3;
+            }
+            else
+            {
+                transform.localPosition = newV3;
+            }
+        }
+        public static void SetLocalScale(this Transform transform, float? x = null, float? y = null, float? z = null)
+        {
+            var newV3 = transform.localScale;
+            newV3.SetXYZ(x, y, z);
+            transform.localScale = newV3;
+        }
+        public static void SetEulerAngles(this Transform transform, Space space, float? x = null, float? y = null, float? z = null)
+        {
+            var newV3 = transform.eulerAngles;
+            newV3.SetXYZ(x, y, z);
+            if(space == Space.World)
+            {
+                transform.rotation = Quaternion.Euler(newV3);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(newV3);
+            }
+        }
 
-		public static void Reset(this Transform t)
-		{
-			Reset(t, TransformElement.All, Space.World);
-		}
-		public static void Reset(this Transform t, TransformElement elements)
-		{
-			Reset(t, elements, Space.World);
-		}
-		public static void Reset(this Transform t, Space space)
-		{
-			Reset(t, TransformElement.All, space);
-		}
-		public static void Reset(this Transform t, TransformElement elements, Space space)
-		{
-			if ((elements & TransformElement.Position) != 0)
-			{
-				if (space == Space.World)
-				{
-					t.position = Vector3.zero;
-				}
-				else
-				{
-					t.localPosition = Vector3.zero;
-				}
-			}
-			if ((elements & TransformElement.Rotation) != 0)
-			{
-				if (space == Space.World)
-				{
-					t.rotation = Quaternion.identity;
-				}
-				else
-				{
-					t.localRotation = Quaternion.identity;
-				}
-			}
-			if ((elements & TransformElement.Scale) != 0)
-			{
-				t.localScale = Vector3.one;
-			}
-		}
-	}
+        public static void Reset(this Transform t)
+        {
+            Reset(t, TransformElement.All, Space.World);
+        }
+        public static void Reset(this Transform t, TransformElement elements)
+        {
+            Reset(t, elements, Space.World);
+        }
+        public static void Reset(this Transform t, Space space)
+        {
+            Reset(t, TransformElement.All, space);
+        }
+        public static void Reset(this Transform t, TransformElement elements, Space space)
+        {
+            if((elements & TransformElement.Position) != 0)
+            {
+                if(space == Space.World)
+                {
+                    t.position = Vector3.zero;
+                }
+                else
+                {
+                    t.localPosition = Vector3.zero;
+                }
+            }
+            if((elements & TransformElement.Rotation) != 0)
+            {
+                if(space == Space.World)
+                {
+                    t.rotation = Quaternion.identity;
+                }
+                else
+                {
+                    t.localRotation = Quaternion.identity;
+                }
+            }
+            if((elements & TransformElement.Scale) != 0)
+            {
+                t.localScale = Vector3.one;
+            }
+        }
+
+        public static Transform FindChildDeep(this Transform t, string name)
+        {
+            var child = t.Find(name);
+            if(child)
+            {
+                return child;
+            }
+            for(int i = 0; i < t.childCount; i++)
+            {
+                FindChildDeep(t.GetChild(i), name);
+            }
+            return null;
+        }
+    }
 }
