6ed47e438b613f360a527552c433f4d2ace059da
Michael Notarnicola <michael@beardphantom.com>
2016-11-18 02:31:20 -0500

    Removing basically useless Actor system.

---
+++ UnityCommonLibrary/Scripts/ScriptableSingleton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace UnityCommonLibrary
+{
+	public abstract class ScriptableSingleton<T> : ScriptableObject where T : ScriptableSingleton<T>
+	{
+		private static bool isShuttingDown;
+		private static T _get;
+
+		public static T get
+		{
+			get
+			{
+				if(isShuttingDown)
+				{
+					return null;
+				}
+				if(!_get)
+				{
+					_get = CreateInstance<T>();
+				}
+				return _get;
+			}
+		}
+		public static void EnsureExists()
+		{
+			if(!_get)
+			{
+				_get = CreateInstance<T>();
+			}
+		}
+	}
+}
