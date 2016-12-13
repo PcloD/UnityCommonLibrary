53da3521aee720f281ecbb989df853c8a476f665
Michael Notarnicola <michael@beardphantom.com>
2016-12-13 11:04:53 -0500

    Find functions

--- UnityCommonLibrary/Utilities/GOUtility.cs
+++ UnityCommonLibrary/Utilities/GOUtility.cs
@@ -11,13 +11,13 @@ namespace UnityCommonLibrary.Utility
 		{
 			Bounds? bounds = null;
 			var colliders = obj.GetComponentsInChildren<Collider>(true);
-			for(int i = 0; i < colliders.Length; i++)
+			for (int i = 0; i < colliders.Length; i++)
 			{
-				if(colliders[i].isTrigger)
+				if (colliders[i].isTrigger)
 				{
 					continue;
 				}
-				if(!bounds.HasValue)
+				if (!bounds.HasValue)
 				{
 					bounds = colliders[i].bounds;
 				}
@@ -26,12 +26,12 @@ namespace UnityCommonLibrary.Utility
 					bounds.Value.Encapsulate(colliders[i].bounds);
 				}
 			}
-			if(!bounds.HasValue)
+			if (!bounds.HasValue)
 			{
 				var renderers = obj.GetComponentsInChildren<Renderer>(true);
-				for(int i = 0; i < renderers.Length; i++)
+				for (int i = 0; i < renderers.Length; i++)
 				{
-					if(!bounds.HasValue)
+					if (!bounds.HasValue)
 					{
 						bounds = renderers[i].bounds;
 					}
@@ -41,14 +41,14 @@ namespace UnityCommonLibrary.Utility
 					}
 				}
 			}
-			if(bounds.HasValue)
+			if (bounds.HasValue)
 			{
 				mask = mask.HasValue ? mask : Physics.DefaultRaycastLayers;
 				var allHits = Physics.RaycastAll(obj.transform.position + Vector3.up * 0.1f, Vector3.down, 1000f, mask.Value);
 				Array.Sort(allHits, (r1, r2) => r1.distance.CompareTo(r2.distance));
-				for(int i = 0; i < allHits.Length; i++)
+				for (int i = 0; i < allHits.Length; i++)
 				{
-					if(!allHits[i].transform.IsChildOf(obj.transform))
+					if (!allHits[i].transform.IsChildOf(obj.transform))
 					{
 						obj.transform.position = allHits[i].point + Vector3.up * (bounds.Value.extents.y - 0.1f);
 						return true;
@@ -60,7 +60,7 @@ namespace UnityCommonLibrary.Utility
 		public static T AddOrGetComponent<T>(this GameObject obj) where T : Component
 		{
 			T component = obj.GetComponent<T>();
-			if(component == null)
+			if (component == null)
 			{
 				return obj.AddComponent<T>();
 			}
@@ -81,7 +81,7 @@ namespace UnityCommonLibrary.Utility
 		public static void SetLayerRecursive(this GameObject obj, int layer)
 		{
 			obj.layer = layer;
-			foreach(Transform child in obj.transform)
+			foreach (Transform child in obj.transform)
 			{
 				child.gameObject.SetLayerRecursive(layer);
 			}
@@ -89,7 +89,7 @@ namespace UnityCommonLibrary.Utility
 		public static string GetPath(GameObject obj)
 		{
 			var sb = new System.Text.StringBuilder("/" + obj.name);
-			while(obj.transform.parent != null)
+			while (obj.transform.parent != null)
 			{
 				obj = obj.transform.parent.gameObject;
 				sb.Insert(0, "/" + obj.name);
@@ -98,9 +98,9 @@ namespace UnityCommonLibrary.Utility
 		}
 		public static void Toggle(bool enabled, params GameObject[] gameObjects)
 		{
-			foreach(var go in gameObjects)
+			foreach (var go in gameObjects)
 			{
-				if(go != null)
+				if (go != null)
 				{
 					go.SetActive(enabled);
 				}
