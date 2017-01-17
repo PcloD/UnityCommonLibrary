0a488d5647bc7bf365e2a35cdc022ab195c146ab
Michael Notarnicola <michael@beardphantom.com>
2017-01-16 22:57:13 -0500

    Services, a dead simple IOC registrar

---
+++ UnityCommonLibrary/Scripts/Services.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityCommonLibrary
+{
+	public static class Services
+	{
+		private static readonly Dictionary<Type, object> registry = new Dictionary<Type, object>();
+
+		public static S Get<S>() where S : class
+		{
+			return (S)registry[typeof(S)];
+		}
+		public static S Register<S, P>() where S : class where P : S, new()
+		{
+			return Register<S>(new P());
+		}
+		public static S Register<S>(S instance) where S : class
+		{
+			registry.Add(typeof(S), instance);
+			return instance;
+		}
+	}
+}
