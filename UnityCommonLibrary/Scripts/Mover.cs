5f3d6f5d03e3b6339c50c97b813c14037cdffdd9
Michael Notarnicola <michael@beardphantom.com>
2017-03-31 03:27:09 -0400

    Exposed Mover fields

--- UnityCommonLibrary/Scripts/Mover.cs
+++ UnityCommonLibrary/Scripts/Mover.cs
@@ -2,39 +2,40 @@
 
 namespace UnityCommonLibrary
 {
-	public class Mover : MonoBehaviour
-	{
-		[SerializeField]
-		private Vector3 movement = Vector3.one;
-		[SerializeField]
-		private Space movementSpace;
-		[SerializeField]
-		private Vector3 rotation = Vector3.one;
-		[SerializeField]
-		private Space rotationSpace;
+    public class Mover : MonoBehaviour
+    {
+        public Vector3 movement = Vector3.one;
+        public Space movementSpace;
+        public Vector3 rotation = Vector3.one;
+        public Space rotationSpace;
+        public bool useRigidbody;
 
-		private new Rigidbody rigidbody;
+        private new Rigidbody rigidbody;
 
-		private void Update()
-		{
-			if(rigidbody)
-			{
-				if(movementSpace == Space.Self)
-				{
-					rigidbody.velocity = transform.TransformVector(movement);
-					rigidbody.angularVelocity = transform.TransformVector(rotation);
-				}
-				else
-				{
-					rigidbody.velocity = movement;
-					rigidbody.angularVelocity = rotation;
-				}
-			}
-			else
-			{
-				transform.Translate(movement, movementSpace);
-				transform.Rotate(rotation, rotationSpace);
-			}
-		}
-	}
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+        }
+        private void Update()
+        {
+            if (useRigidbody && rigidbody != null)
+            {
+                if (movementSpace == Space.Self)
+                {
+                    rigidbody.velocity = transform.TransformVector(movement);
+                    rigidbody.angularVelocity = transform.TransformVector(rotation);
+                }
+                else
+                {
+                    rigidbody.velocity = movement;
+                    rigidbody.angularVelocity = rotation;
+                }
+            }
+            else
+            {
+                transform.Translate(movement, movementSpace);
+                transform.Rotate(rotation, rotationSpace);
+            }
+        }
+    }
 }
